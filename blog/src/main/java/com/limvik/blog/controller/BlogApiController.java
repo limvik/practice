0d7b38be47378b25f780911217ac230ecb53f8fb
limvik <seongguk.dev@gmail.com>
2023-05-19 20:36:01 +0900

    Add feature: API for get all articles

--- blog/src/main/java/com/limvik/blog/controller/BlogApiController.java
+++ blog/src/main/java/com/limvik/blog/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.limvik.blog.controller;
 
 import com.limvik.blog.domain.Article;
 import com.limvik.blog.dto.AddArticleRequest;
+import com.limvik.blog.dto.ArticleResponse;
 import com.limvik.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +25,16 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- blog/src/main/java/com/limvik/blog/service/BlogService.java
+++ blog/src/main/java/com/limvik/blog/service/BlogService.java
@@ -6,13 +6,18 @@ import com.limvik.blog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
-
     private final BlogRepository blogRepository;
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
