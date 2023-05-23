cc87e582460322d9e83e97c6ce9a782a5282093e
limvik <seongguk.dev@gmail.com>
2023-05-23 23:26:18 +0900

    Add feat: token for authentication

---
+++ blog/src/main/java/com/limvik/blog/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.limvik.blog.repository;
+
+import com.limvik.blog.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
