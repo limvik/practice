a24237a3018a29b922b5a7dbb164728d552c9e1f
limvik <seongguk.dev@gmail.com>
2023-08-30 15:19:13 +0900

    List rendering
    with v-for and attribute key

--- vue/intro/main.js
+++ vue/intro/main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             url: 'https://v3-docs.vuejs-korea.org/',
             inventory: 10,
             inStock: true,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['L', 'M', 'S']
         }
     }
 })
