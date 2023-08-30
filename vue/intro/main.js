3d43a42f80f9d274854ddca471a8f7001185e6b1
limvik <seongguk.dev@gmail.com>
2023-08-30 13:47:14 +0900

    Conditional rendering
    with v-show, v-if, v-else-if, v-else

--- vue/intro/main.js
+++ vue/intro/main.js
@@ -3,7 +3,10 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://v3-docs.vuejs-korea.org/'
+            url: 'https://v3-docs.vuejs-korea.org/',
+            inventory: 10,
+            inStock: true,
+            onSale: true
         }
     }
 })
