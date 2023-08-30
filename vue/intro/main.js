b901acf66d5759a81d1dca1e32f225ceb329da8d
limvik <seongguk.dev@gmail.com>
2023-08-30 13:36:10 +0900

    Attribute binding with v-bind

---
+++ vue/intro/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://v3-docs.vuejs-korea.org/'
+        }
+    }
+})
