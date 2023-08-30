99d9fe5874caa2412bcb41e8c7301be3cec65bc2
limvik <seongguk.dev@gmail.com>
2023-08-30 20:34:21 +0900

    Class and style binding
    class with boolean, ternary operator and style with boolean, data

--- vue/intro/main.js
+++ vue/intro/main.js
@@ -4,16 +4,20 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'https://v3-docs.vuejs-korea.org/',
-            inventory: 10,
-            inStock: true,
-            onSale: true,
+            inventory: 0,
+            inStock: false,
+            onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
             sizes: ['L', 'M', 'S'],
-            cart: 0
+            cart: 0,
+            styles: {
+                color: 'red',
+                fontSize: '2em'
+            }
         }
     },
     methods: {
