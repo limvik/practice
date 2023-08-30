249a09611398876733aaef5d7fe50acc688b2408
limvik <seongguk.dev@gmail.com>
2023-08-30 15:45:11 +0900

    Event handling
    with v-on, @click, @mouseover

--- vue/intro/main.js
+++ vue/intro/main.js
@@ -9,10 +9,23 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
-            sizes: ['L', 'M', 'S']
+            sizes: ['L', 'M', 'S'],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart++
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            if (this.cart > 0)
+                this.cart--
         }
     }
 })
